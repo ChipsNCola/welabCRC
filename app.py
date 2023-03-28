0e1500976ee88f91b993c49ff55bd2a0317de2d7
Chips009 <Chips009@MacBook-Air-de-Chips.local>
2023-03-28 09:56:55 +0000

    fix for app.py

--- app.py
+++ app.py
@@ -4,19 +4,21 @@ import numpy as np
 import torch
 from transformers import TextClassificationPipeline
 from transformers import BertForSequenceClassification
+from transformers import BertTokenizer
 
 np.random.seed(1337)
 
 app = Flask(__name__)
-
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', 
+                                          do_lower_case=True)
 model = BertForSequenceClassification.from_pretrained("bert-base-uncased",
                                                       num_labels=3,
                                                       output_attentions=False,
                                                       output_hidden_states=False)
-model.load_state_dict(torch.load("./welabCRC/model/finetuned_BERT_epoch_3.model"))
+model.load_state_dict(torch.load("./model/finetuned_BERT_epoch_3.model",map_location=torch.device('cpu')))
 
 def get_label(text):
-    pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores=False,device="cuda:0")
+    pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores=False,device="cpu")
     output=pipe(text)[0]['label']
     if output=='LABEL_0':
         label="Compte"
@@ -30,14 +32,14 @@ def get_label(text):
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
-    """ Prediction function."""
-    response = request.form['text']
-    prediction_tag=get_label(response)
-    return render_template('index.html', text=prediction_tag, submission=response)
+    if request.method == 'POST':
+        response = request.form['text']
+        prediction_tag=get_label(response)
+        return render_template('index.html', text=prediction_tag, submission=response)
 
     if request.method == 'GET':
         return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000,debug=False)
+    app.run(debug=False)
